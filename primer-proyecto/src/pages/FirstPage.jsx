8ffb67a4151e7fab1ec6c350a2f8c38a17ac76cc
SleepyAsh120 <nicofelovi@gmail.com>
2022-11-01 16:49:55 -0500

    Cambio de titulo

--- primer-proyecto/src/pages/FirstPage.jsx
+++ primer-proyecto/src/pages/FirstPage.jsx
@@ -17,7 +17,8 @@ export const FirstPage = () => {
     }
     return(
         <>
-            <h1>Contador</h1>
+            <h1>Contador mejorado</h1>
+            <hr/>
             <h1>{valor}</h1>
             <button onClick={() => modificarEstado(+1)}>SUMA 1</button>  
             <button onClick={() => modificarEstado(-1)}>RESTA 1</button> 
